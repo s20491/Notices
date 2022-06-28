98972ed95767110e83c004338c7960f256c23b89
KrzysztofLewandowskii <kris741999@gmail.com>
2022-06-28 21:16:54 +0200

    ImageRepository is done

--- Notices.Infrastructure/Repository/ImageRepository.cs
+++ Notices.Infrastructure/Repository/ImageRepository.cs
@@ -1,31 +1,80 @@
+using Microsoft.EntityFrameworkCore;
+using Notices.Infrastructure.Context;
 using Notices.Infrastructure.Entities;
+using Notices.Infrastructure.Exceptions;
 
 namespace Notices.Infrastructure.Repository;
 
 public class ImageRepository : IImageRepository
 {
-    public Task<IEnumerable<Image>> GetAll()
+    private readonly MainContext _mainContext;
+
+    public ImageRepository(MainContext mainContext)
     {
-        throw new NotImplementedException();
+        _mainContext = mainContext;
     }
 
-    public Task<Image> GetById(int id)
+    public async Task<IEnumerable<Image>> GetAll()
     {
-        throw new NotImplementedException();
+        var images = await _mainContext.Image.ToListAsync();
+
+        foreach (var image in images)
+        {
+            await _mainContext.Entry(image).Reference(x => x.Notice).LoadAsync();
+        }
+
+        return images;
     }
 
-    public Task Add(Image entity)
+    public async Task<Image> GetById(int id)
     {
-        throw new NotImplementedException();
+        var image = await _mainContext.Image.SingleOrDefaultAsync(x => x.Id == id);
+        if (image != null)
+        {
+            await _mainContext.Entry(image).Reference(x => x.Notice)
+                .LoadAsync();
+            return image;
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
     }
 
-    public Task Update(Image entity)
+    public async Task Add(Image entity)
     {
-        throw new NotImplementedException();
+        entity.DateOfCreation = DateTime.UtcNow;
+        await _mainContext.AddAsync(entity);
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task DeleteById(int id)
+    public async Task Update(Image entity)
     {
-        throw new NotImplementedException();
+        var imageToUpdate = await _mainContext.Image.SingleOrDefaultAsync(x => x.Id == entity.Id);
+
+        if (imageToUpdate == null)
+        {
+            throw new EntityNotFoundException();
+        }
+
+        imageToUpdate.Notice = entity.Notice;
+        imageToUpdate.Data = entity.Data;
+        imageToUpdate.DateOfUpdate = DateTime.UtcNow;
+
+        await _mainContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteById(int id)
+    {
+        var imageToDelete = await _mainContext.Image.SingleOrDefaultAsync(x => x.Id == id);
+        if (imageToDelete != null)
+        {
+            _mainContext.Image.Remove(imageToDelete);
+            await _mainContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
     }
 }
