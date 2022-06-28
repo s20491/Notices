e2402bd902f7cab6014da1717620b1ccca3a20e0
KrzysztofLewandowskii <kris741999@gmail.com>
2022-06-28 21:13:50 +0200

    AddressRepository is done

--- Notices.Infrastructure/Repository/AddressRepository.cs
+++ Notices.Infrastructure/Repository/AddressRepository.cs
@@ -1,31 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using Notices.Infrastructure.Context;
 using Notices.Infrastructure.Entities;
+using Notices.Infrastructure.Exceptions;
 
 namespace Notices.Infrastructure.Repository;
 
 public class AddressRepository : IAddressRepository
 {
-    public Task<IEnumerable<Address>> GetAll()
+    private readonly MainContext _mainContext;
+
+    public AddressRepository(MainContext mainContext)
     {
-        throw new NotImplementedException();
+        _mainContext = mainContext;
+    }
+    public async Task<IEnumerable<Address>> GetAll()
+    {
+        var addresses = await _mainContext.Address.ToListAsync();
+        return addresses;
     }
 
-    public Task<Address> GetById(int id)
+    public async Task<Address> GetById(int id)
     {
-        throw new NotImplementedException();
+        var address = await _mainContext.Address.SingleOrDefaultAsync(x => x.Id == id);
+        if (address == null)
+        {
+            throw new EntityNotFoundException();
+        }
+        else
+        {
+            return address;
+        }
+        
     }
 
-    public Task Add(Address entity)
+    public async Task Add(Address entity)
     {
-        throw new NotImplementedException();
+        entity.DateOfCreation = DateTime.UtcNow;
+        await _mainContext.AddAsync(entity);
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task Update(Address entity)
+    public async Task Update(Address entity)
     {
-        throw new NotImplementedException();
+        var addressToUpdate = await _mainContext.Address.SingleOrDefaultAsync(x => x.Id == entity.Id);
+
+        if (addressToUpdate == null)
+        {
+            throw new EntityNotFoundException();
+        }
+
+        addressToUpdate.City = entity.City;
+        addressToUpdate.Country = entity.Country;
+        addressToUpdate.Street = entity.Street;
+        addressToUpdate.ApartmentNumber = entity.ApartmentNumber;
+        addressToUpdate.BuildingNumber = entity.BuildingNumber;
+        addressToUpdate.ZipCode = entity.ZipCode;
+        addressToUpdate.DateOfUpdate = DateTime.UtcNow;
+
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task DeleteById(int id)
+    public async Task DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var addressToDelete = await _mainContext.Address.SingleOrDefaultAsync(x => x.Id == id);
+        if (addressToDelete != null)
+        {
+            _mainContext.Address.Remove(addressToDelete);
+            await _mainContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
     }
 }
