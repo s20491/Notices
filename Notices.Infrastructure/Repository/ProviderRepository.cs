55c3c1ca1c0fe8eccdfe99cc80d10b38ee7b5389
KrzysztofLewandowskii <kris741999@gmail.com>
2022-06-28 21:24:11 +0200

    ProviderRepository is done

--- Notices.Infrastructure/Repository/ProviderRepository.cs
+++ Notices.Infrastructure/Repository/ProviderRepository.cs
@@ -1,31 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using Notices.Infrastructure.Context;
 using Notices.Infrastructure.Entities;
+using Notices.Infrastructure.Exceptions;
 
 namespace Notices.Infrastructure.Repository;
 
 public class ProviderRepository : IProviderRepository
 {
-    public Task<IEnumerable<Provider>> GetAll()
+    private readonly MainContext _mainContext;
+
+    public ProviderRepository(MainContext mainContext)
     {
-        throw new NotImplementedException();
+        _mainContext = mainContext;
+    }
+    
+    public async Task<IEnumerable<Provider>> GetAll()
+    {
+        var providers = await _mainContext.Provider.ToListAsync();
+
+        foreach (var provider in providers)
+        {
+            await _mainContext.Entry(provider).Reference(x => x.Notice).LoadAsync();
+        }
+
+        return providers;
     }
 
-    public Task<Provider> GetById(int id)
+    public async Task<Provider> GetById(int id)
     {
-        throw new NotImplementedException();
+        var provider = await _mainContext.Provider.SingleOrDefaultAsync(x => x.Id == id);
+        if (provider != null)
+        {
+            await _mainContext.Entry(provider).Reference(x => x.Notice).LoadAsync();
+            return provider;
+        }
+
+        throw new EntityNotFoundException();
     }
 
-    public Task Add(Provider entity)
+    public async Task Add(Provider entity)
     {
-        throw new NotImplementedException();
+        var provider = await _mainContext.Provider.SingleOrDefaultAsync(x => x.Notice == entity.Notice);
+
+        if (provider != null)
+        {
+            throw new EntityAlreadyExistException();
+        }
+
+        entity.DateOfCreation = DateTime.UtcNow;
+        await _mainContext.AddAsync(entity);
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task Update(Provider entity)
+    public async Task Update(Provider entity)
     {
-        throw new NotImplementedException();
+        var providerToUpdate = await _mainContext.Provider.SingleOrDefaultAsync(x => x.Id == entity.Id);
+
+        if (providerToUpdate == null)
+        {
+            throw new EntityNotFoundException();
+        }
+
+        providerToUpdate.Account = entity.Account;
+        providerToUpdate.Notice = entity.Notice;
+        providerToUpdate.DateOfUpdate = DateTime.UtcNow;
+
+        await _mainContext.SaveChangesAsync();
     }
 
-    public Task DeleteById(int id)
+    public async Task DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var providerToDelete = await _mainContext.Provider.SingleOrDefaultAsync(x => x.Id == id);
+        if (providerToDelete != null)
+        {
+            _mainContext.Provider.Remove(providerToDelete);
+            await _mainContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new EntityNotFoundException();
+        }
     }
 }
